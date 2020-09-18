bd56f983b2dad6c78a5c7570a5c6a05743a6357b
fakehydra <1578259905@qq.com>
2020-09-18 10:48:41 +0800

    ref

--- project/tests/test_config.py
+++ project/tests/test_config.py
@@ -4,16 +4,16 @@ from project import app
 
 class TestDevelopmentConfig(TestCase):
     def create_app(self):
-        app.config.from_objuect('project.config.DevelopmentConfig')
+        app.config.from_object('project.config.DevelopmentConfig')
         return app
 
     def test_app_is_development(self):
-        self.assertTure(app.config['SECRET_KEY'] == 'secret')
-        self.assertTure(app.config['DEBUG'] is True)
+        self.assertTrue(app.config['SECRET_KEY'] == 'secret')
+        self.assertTrue(app.config['DEBUG'] is True)
         self.assertFalse(current_app is None)
-        self.assertTure(
-                app.config['SQLALCHEMY_DATABASE_URI'] ==
-                'mysql+pymysql://root:root321@users-db:3306/users_dev'
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'mysql+pymysql://root:root321@users-db:3306/users_dev'
         )
 
 class TestTestingConfig(TestCase):
@@ -22,13 +22,15 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertTure(app.config['SECRET_KEY'] == 'secret')
-        self.assertTure(app.config['DEBUG'])
-        self.assertTure(app.config['TESTING'])
+        #self.assertTrue(app.config['SECRET_KEY'] == 'secret')
+        self.assertTrue(app.config['SECRET_KEY'] == 'secret')
+        #self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-        self.assertTure(
-                app.config['SQLALCHEMY_DATABASE_URI'] ==
-                'mysql+pymysql://root:root321@users-db:3306/users_test'
+        self.assertTrue(
+            app.config['SQLALCHEMY_DATABASE_URI'] ==
+            'mysql+pymysql://root:root321@users-db:3306/users_test'
         )
 
 class TestProductionConfig(TestCase):
@@ -37,6 +39,6 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTure(app.config['SECRET_KEY'] == 'secret')
+        self.assertTrue(app.config['SECRET_KEY'] == 'secret')
         self.assertFalse(app.config['DEBUG'])
         self.assertFalse(app.config['TESTING'])
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -4,7 +4,7 @@ from project.tests.base import BaseTestCase
 class TestUserService(BaseTestCase):
     def test_users(self):
         """确保ping的服务正常"""
-        reponse = self.client.get('/ping')
+        response = self.client.get('/ping')
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong', data['message'])
